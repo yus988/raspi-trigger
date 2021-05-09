f6dc5fcbc0bdf2a064c599f8d5454fccbbf37fe6
yus988 <yus988@hapbeat.com>
2021-05-09 20:02:11 +0900

    add input selection func

--- se.py
+++ se.py
@@ -3,15 +3,31 @@ import time
 import RPi.GPIO as GPIO
 from time import perf_counter
 
+# modules for restart python
+import os
+import sys
+
 pygame.mixer.quit()
-pygame.mixer.pre_init(buffer=32)
+pygame.mixer.pre_init(buffer=64)
 pygame.mixer.init()
 
-se = pygame.mixer.Sound('gunRaw.wav')
-se = pygame.mixer.Sound('gunRawArm.wav')
-se = pygame.mixer.Sound('gunRawNeck.wav')
-se = pygame.mixer.Sound('gunSamp.wav')
-se = pygame.mixer.Sound('gunStime.wav')
+arrName = [
+    'gunRaw.wav',
+    'gunRawArm.wav',
+    'gunRawNeck.wav',
+    'gunSamp.wav',
+    'gunStime.wav'
+]
+
+se1 = pygame.mixer.Sound(arrName[0])
+se2 = pygame.mixer.Sound(arrName[1])
+se3 = pygame.mixer.Sound(arrName[2])
+se4 = pygame.mixer.Sound(arrName[3])
+se5 = pygame.mixer.Sound(arrName[4])
+
+
+# arrSe = pygame.sndarray.samples(sndArr)
+# print(arrSe)
 
 # Pin Number
 TACT_GPIO = 19
@@ -19,20 +35,49 @@ TACT_GPIO = 19
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(TACT_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-def playSE():
-    se.play() # loop count
-    time.sleep(0.5)   #20秒再生
-    pygame.mixer.music.stop()  #停止
+
+def playSE(se):
+    se.play()  # loop count
+    time.sleep(0.5)  # 一定時間再生
+    pygame.mixer.music.stop()  # 停止
 
 # GPIO.add_event_detect(TACT_GPIO, GPIO.RISING, callback=playSE, bouncetime=1)
 
-try: 
+
+seType = input("input type num : ")
+# se = pygame.mixer.Sound('gunRaw.wav')
+
+# se = "hoge"
+
+if seType == "1":
+    se = se1
+    name = arrName[0]
+elif seType == "2":
+    se = se2
+    name = arrName[1]
+elif seType == "3":
+    se = se3
+    name = arrName[2]
+elif seType == "4":
+    se = se4
+    name = arrName[3]
+elif seType == "5":
+    se = se5
+    name = arrName[4]
+
+print(name)
+
+
+try:
     while True:
-        # time.sleep(1) 
+        # time.sleep(1)
         if GPIO.input(TACT_GPIO) == 0:
             # print("before play",perf_counter())
-            playSE()
-            
+            playSE(se)
+
 except KeyboardInterrupt:
-        GPIO.cleanup()
-        print("Finished and clean up GPIO")
+    GPIO.cleanup()
+    print("Finished and clean up GPIO")
+    # estart python
+    sys.stdout.flush()
+    os.execv(sys.argv[0], sys.argv)
